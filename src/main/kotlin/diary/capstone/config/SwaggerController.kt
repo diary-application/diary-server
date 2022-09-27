24e31e56a8304d28f9252295b14ee4ba3e1e9b0e
Seong Heung Bae <mopil1102@gmail.com>
2022-09-27 23:46:19 +0900

    CI/CD test

--- src/main/kotlin/diary/capstone/config/SwaggerController.kt
+++ src/main/kotlin/diary/capstone/config/SwaggerController.kt
@@ -15,5 +15,5 @@ class SwaggerController {
 @RestController
 class GlobalController {
     @GetMapping
-    fun welcome() = "diary server is running super sexy!!"
+    fun welcome() = "diary server is running super !!"
 }
