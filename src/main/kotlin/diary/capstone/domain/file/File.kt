6ff7caecfe5b318abe1d1a042d52bdb0fb964e08
5pponent <ahdwjdtprtm@gmail.com>
2022-10-25 19:09:05 +0900

    fix: 유저 파일 삭제 오류 수정

--- src/main/kotlin/diary/capstone/domain/file/File.kt
+++ src/main/kotlin/diary/capstone/domain/file/File.kt
@@ -1,6 +1,7 @@
 package diary.capstone.domain.file
 
 import diary.capstone.domain.feed.Feed
+import diary.capstone.domain.user.User
 import javax.persistence.*
 
 @Entity
@@ -18,12 +19,18 @@ class File (
     // 파일 타입 구분(피드 첨부 이미지, 프로필 이미지..)
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    var profileImage: File? = null,
+    var userFile: User? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id")
     var feedFile: Feed? = null
 ) {
+    // 파일이 등록된 유저 설정
+    fun setUser(user: User): File {
+        this.userFile = user
+        return this
+    }
+
     // 파일이 등록된 피드 설정
     fun setFeed(feed: Feed): File {
         this.feedFile = feed
