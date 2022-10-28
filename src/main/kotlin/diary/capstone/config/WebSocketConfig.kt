734a7ecf551624fc9aac74cfbbd3c654f4d82d9d
5pponent <ahdwjdtprtm@gmail.com>
2022-10-28 21:40:02 +0900

    feat: websocket 기본 기능 추가

---
+++ src/main/kotlin/diary/capstone/config/WebSocketConfig.kt
@@ -0,0 +1,17 @@
+package diary.capstone.config
+
+import diary.capstone.domain.chat.ChatHandler
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+
+@Configuration
+@EnableWebSocket
+class WebSocketConfig(private val chatHandler: ChatHandler): WebSocketConfigurer {
+
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry
+            .addHandler(chatHandler, "ws/chat").setAllowedOrigins("*")
+    }
+}
