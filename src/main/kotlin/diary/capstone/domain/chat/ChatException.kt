73a58f340c1881dd208b491812bf1abe52e22c1c
5pponent <ahdwjdtprtm@gmail.com>
2022-10-31 06:42:02 +0900

    feat: 채팅 읽기 기능 추가

--- src/main/kotlin/diary/capstone/domain/chat/ChatException.kt
+++ src/main/kotlin/diary/capstone/domain/chat/ChatException.kt
@@ -1,5 +1,7 @@
 package diary.capstone.domain.chat
 
 const val CHAT_SESSION_NOT_FOUND = "해당 채팅 세션을 찾을 수 없습니다."
+const val CHAT_NOT_FOUND = "해당 채팅을 찾을 수 없습니다."
+const val CHAT_SESSION_ACCESS_DENIED = "해당 채팅 세션에 접근할 권한이 없습니다."
 
 class ChatException(message: String): RuntimeException(message)
