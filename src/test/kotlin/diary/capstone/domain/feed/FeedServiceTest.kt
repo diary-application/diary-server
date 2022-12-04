48c4a3ca7f7125b6087f3521c9226262c9ca13a7
5pponent <ahdwjdtprtm@gmail.com>
2022-12-05 04:03:14 +0900

    test: 피드 쿼리 성능 테스트 코드 작성

--- src/test/kotlin/diary/capstone/domain/feed/FeedServiceTest.kt
+++ src/test/kotlin/diary/capstone/domain/feed/FeedServiceTest.kt
@@ -1,6 +1,7 @@
 package diary.capstone.domain.feed
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import diary.capstone.domain.feed.comment.CommentResponse
 import diary.capstone.domain.user.UserService
 import org.junit.jupiter.api.Test
 
@@ -8,120 +9,133 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.transaction.annotation.Transactional
-import diary.capstone.domain.feed.QFeed.feed
-import diary.capstone.domain.file.QFile.file
-import diary.capstone.domain.feed.QFeedLike.feedLike
-import diary.capstone.domain.user.QUser.user
-import diary.capstone.domain.user.User
 
 @SpringBootTest
 @Transactional
 internal class FeedServiceTest {
 
-    @Autowired lateinit var feedService: FeedService
-    @Autowired lateinit var feedRepository: FeedRepository
     @Autowired lateinit var userService: UserService
-    @Autowired lateinit var userRepository: FeedRepository
+    @Autowired lateinit var qFeedRepository: QFeedRepository
     @Autowired lateinit var jpaQueryFactory: JPAQueryFactory
 
-    @Test @DisplayName("내 피드 모두 조회")
-    fun getFeedsMy() {
-        val userId = 1L
-        val user = userService.getUserById(userId)
-        val pageable = PageRequest.of(0, 5)
-        val result = feedService.getFeeds(pageable, userId, null, user)
-        result.content.forEach {
-            println(it.id.toString() + ": " + it.content)
+    /**
+     * - files, likes, comments 가 해당 엔티티.feed_id IN (result 피드 id 목록 pageSize 만큼) Batch 쿼리로 처리
+     * - isFollowed 를 얻기 위해 로그인 유저의 following 정보를 불러오는 데에 일반 쿼리 처리
+     * - select Follow from Follow following where following.user_id = 로그인 유저 ID;
+     * - 피드 페이징 목록: 2(페이징) + 1(following) + 1(files) + 1(likes) + 1(comments) = 6개의 쿼리가 실행
+     */
+    private fun feedTestResult(feedList: Page<FeedResponse>) {
+        println("currentPage: " + (feedList.number + 1))
+        println("totalPages: " + feedList.totalPages)
+        println("totalElements: " + feedList.totalElements)
+        feedList.content.forEach {
+            println("Feed (${it.id}) =====================================================")
+            println("files: ${it.files.map { file -> file.id }}")
+            println("likeCount: ${it.likeCount}")
+            println("isLiked: ${it.isLiked}")
+            println("commentCount: ${it.commentCount}")
+            println("isFollowed = ${it.isFollowed}")
         }
     }
-    @Test @DisplayName("다른 유저 피드 모두 조회")
-    fun getFeedsOther() {
-        val userId = 1L
-        val user = userService.getUserById(userId)
-        val pageable = PageRequest.of(0, 5)
-        val result = feedService.getFeeds(pageable, 2L, null, user)
-        result.content.forEach {
-            println(it.id.toString() + ": " + it.content)
+
+    /**
+     * - children: children.parent_id IN (result 댓글 id 목록 pageSize 만큼) Batch
+     * - likes: likes.comment_id IN (result 댓글 id 목록 pageSize 만큼) Batch
+     * - isFollowed 를 얻기 위해 로그인 유저의 following 정보를 불러오는 데에 일반 쿼리 처리
+     * - select Follow from Follow following where following.user_id = 로그인 유저 ID;
+     * - 댓글 페이징 목록: 2(페이징) + 1(following) + 1(children) + 1(likes) = 5개의 쿼리 실행
+     */
+    private fun commentTestResult(commentList: Page<CommentResponse>) {
+        println("currentPage: " + (commentList.number + 1))
+        println("totalPages: " + commentList.totalPages)
+        println("totalElements: " + commentList.totalElements)
+        commentList.content.forEach {
+            println("Comment (${it.id}) =====================================================")
+            println("${it.content}")
+            println("childCount = ${it.childCount}")
+            println("isFollowed = ${it.isFollowed}")
+            println("isLiked = ${it.isLiked}")
+            println("likeCount = ${it.likeCount}")
+            println("parentId = ${it.parentId}")
         }
     }
+
+    @Test @DisplayName("특정 유저의 피드 페이징 조회")
+    fun getUserFeeds() {
+        val loginUser = userService.getUserById(1L)
+        val pageable = PageRequest.of(0, 10)
+        val result = qFeedRepository.findFeedsByUserId(pageable, 6L, loginUser)
+
+        feedTestResult(result)
+    }
+
     @Test @DisplayName("전체 공개 피드 모두 조회")
     fun getFeedsAll() {
-        val userId = 1L
-        val user = userService.getUserById(userId)
-        val pageable = PageRequest.of(0, 5)
-        val result = feedService.getFeeds(pageable, null, null, user)
-        result.content.forEach {
-            println(it.id.toString() + ": " + it.content)
-        }
+        val loginUser = userService.getUserById(1L)
+        val pageable = PageRequest.of(0, 10)
+        val result = qFeedRepository.findFeedsByShowScope(pageable, SHOW_ALL, loginUser)
+
+        feedTestResult(result)
     }
 
     @Test @DisplayName("피드 검색")
     fun searchFeedsByUserAndKeyword() {
-        val userId = 1L
+        val loginUser = userService.getUserById(1L)
         val keyword = "저"
         val pageable = PageRequest.of(0, 2)
+        val result = qFeedRepository.findFeedsByUserIdAndKeyword(pageable, loginUser.id!!, keyword, loginUser)
 
-        // querydsl fetch join 으로 해결, 페이징은 Pageable 의 offset, pageSize 로 해결
-        val qResult = jpaQueryFactory
-            .selectFrom(feed).distinct()
-            .leftJoin(feed.files, file).fetchJoin()
-            .where(
-                feed.writer.id.eq(userId).and(
-                    file.description.like("%$keyword%").or(feed.content.like("%$keyword%"))
-                )
-            )
-            .orderBy(feed.id.desc())
-            .offset(pageable.offset)
-            .limit(pageable.pageSize.toLong())
-            .fetch()
-
-        qResult.forEach {
-            println(it.id)
-        }
-    }
-
-    @Test @DisplayName("피드 하나 조회")
-    fun getFeed() {
-        val feedId = 1L
-        val feed = feedService.getFeed(feedId)
+        feedTestResult(result)
     }
 
     @Test @DisplayName("피드 좋아요 목록 조회")
     fun getFeedLikes() {
         val feedId = 1L
-        val pageable = PageRequest.of(0, 2)
+        val pageable = PageRequest.of(0, 3)
+        val likedUsers = qFeedRepository.findFeedLikeUsers(pageable, feedId)
+        println("피드 좋아요 수: ${likedUsers.totalElements}")
 
-        val qResult = jpaQueryFactory
-            .select(feedLike.user.id)
-            .from(feedLike)
-            .where(feedLike.feed.id.eq(feedId))
-            .fetch()
-
-        val likedUsers = jpaQueryFactory
-            .selectFrom(user)
-            .where(user.id.`in`(qResult))
-            .fetch()
-
-        likedUsers.forEach {
-            println(it.name)
-        }
+        likedUsers.forEach { println(it.name + ": " + it.profileImage?.source) }
     }
     
     @Test @DisplayName("루트 댓글 목록 조회")
     fun getRootComments() {
+        val loginUser = userService.getUserById(1L)
+        val feedId = 193L
+        val pageable = PageRequest.of(0, 10)
+        val result = qFeedRepository.findRootCommentsByFeedId(pageable, feedId, loginUser)
+
+        commentTestResult(result)
     }
 
-    @Test @DisplayName("내 댓글 목록 조회")
+    @Test @DisplayName("특정 유저 루트 댓글 목록 조회")
     fun getMyComments() {
+        val loginUser = userService.getUserById(1L)
+        val feedId = 139L
+        val pageable = PageRequest.of(0, 1)
+        val result = qFeedRepository.findRootCommentsByFeedIdAndUserId(pageable, feedId, loginUser.id!!, loginUser)
+
+        commentTestResult(result)
     }
 
     @Test @DisplayName("대댓글 목록 조회")
     fun getChildComments() {
+        val loginUser = userService.getUserById(1L)
+        val commentId = 224L
+        val pageable = PageRequest.of(0, 1)
+        val result = qFeedRepository.findChildCommentsByParentCommentId(pageable, commentId, loginUser)
+
+        commentTestResult(result)
     }
 
-    @Test @DisplayName("댓글 좋아요 목록 조회")
+    @Test @DisplayName("댓글 좋아요 수 조회")
     fun getCommentLikes() {
+        val commentId = 226L
+
+        val result = qFeedRepository.getCommentLikeCount(commentId)
+        println("$commentId LikeCount: $result")
     }
 }
